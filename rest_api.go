f1c9d07d75a51718ff9178be3f1907001ae5c1ff
Prativa Chakraborty <68401118+PrativaChakraborty@users.noreply.github.com>
2023-04-03 22:32:01 +0530

    second commit

---
+++ rest_api.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"CompareGo/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*") // load all the html files in the templates folder
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "API is working",
+		})
+	})
+	r.GET("/search", func(c *gin.Context) {
+		query := c.Query("q")
+		amazon_result := services.GetAmazon(query)
+		flipkart_result := services.GetFlipkart(query)
+		c.JSON(200, gin.H{
+			"query":    query,
+			"amazon":   amazon_result,
+			"flipkart": flipkart_result,
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
---
+++ services/webScrapping.go
@@ -0,0 +1,64 @@
+package services
+
+import (
+	"log"
+	"strings"
+
+	"github.com/anaskhan96/soup"
+)
+
+func GetAmazon(query string) map[string]string {
+	var item = make(map[string]string)
+	query = strings.Replace(query, " ", "+", -1)
+	// add headers to the request
+	soup.Header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36")
+	res, err := soup.Get("https://www.amazon.in/s?k=" + query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println(res)
+	doc := soup.HTMLParse(res)
+	soup.SetDebug(true)
+	//fmt.Println(doc.FullText())
+	block := doc.Find("div", "data-component-type", "s-search-result")
+	item["title"] = block.Find("span", "class", "a-size-medium").Text()
+	item["price"] = block.Find("span", "class", "a-price-whole").Text()
+	if strings.Contains(item["title"], query) {
+		return item
+	} else {
+		b := map[string]string{
+			"message": "Item not found in amazon",
+		}
+		return b
+	}
+
+}
+func GetFlipkart(query string) map[string]string {
+	var item = make(map[string]string)
+	query = strings.Replace(query, " ", "+", -1)
+	soup.Header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36")
+	res, err := soup.Get("https://www.flipkart.com/search?q=" + query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc := soup.HTMLParse(res)
+	soup.SetDebug(true)
+	block := doc.Find("a", "class", "_1fQZEK")
+	item["title"] = block.Find("div", "class", "_4rR01T").Text()
+	item["price"] = block.Find("div", "class", "_30jeq3").Text()
+	if strings.Contains(item["title"], query) {
+		return item
+	} else {
+		b := map[string]string{
+			"message": "Item not found in flipkart",
+		}
+		return b
+	}
+}
+
+// func main() {
+// 	// fmt.Print("Hello World")
+// 	fmt.Print(getAmazon("iphone 11"))
+// 	fmt.Print("\n")
+// 	fmt.Print(getFlipkart("iphone 11"))
+// }
